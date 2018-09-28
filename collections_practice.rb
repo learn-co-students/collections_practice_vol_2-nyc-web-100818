ebc4885569f66740d524e605cb8ae472a4471916
Eric Laitman <ericlaitman@Erics-MacBook-Pro-2.local>
2018-09-28 15:10:03 -0400

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -30,7 +30,6 @@ end
 
 def count_elements(array)
 array.each do |original_hash|
-    binding.pry
     original_hash[:count] = 0
     name = original_hash[:name]
     array.each do |hash|
@@ -40,3 +39,56 @@ array.each do |original_hash|
     end
   end.uniq
 end
+
+# [{:first_name=>"blake"=>{:awesomeness=>10, :height=>"74", :last_name=>"johnson"},},
+#    {:first_name=>"ashley"=>{:awesomeness=>9, :height=>60, :last_name=>"dubs"}}]
+
+def merge_data(keys, values)
+  container = []
+  keys.each do |person_name|
+    name = person_name[:first_name]
+    values.each do |person_data|
+      if person_data[name]
+        merged_person = person_data[name]
+        merged_person[:first_name] = name
+        container << merged_person
+      end
+    end
+  end
+  container
+end
+
+def find_cool(cool)
+cool.each do |x|
+  x.each do |y,z|
+if z == "cool"
+  return [x]
+  end
+end
+end
+end
+
+def organize_schools(schools)
+  nyc = []
+  sf = []
+  chicago = []
+schools.each do |x,y|
+  y.each do |a,z|
+    if z == "Chicago"
+      chicago << x
+    elsif z == "NYC"
+      nyc << x
+    elsif z == "SF"
+      sf << x
+    end
+  end
+end
+{"NYC" => nyc, "SF" => sf, "Chicago" => chicago}
+
+end
+
+# let(:organized_schools) {
+#   {"NYC"=>["flatiron school bk", "flatiron school", "general assembly"],
+#    "SF"=>["dev boot camp", "Hack Reactor"],
+#    "Chicago"=>["dev boot camp chicago"]}
+# }
